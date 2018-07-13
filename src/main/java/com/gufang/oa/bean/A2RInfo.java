16d807799dccf695d5e8f82c79a7ff2b79efd9b2
qixiangchen <qixiang_chen@aliyun.com>
2018-07-13 21:18:59 +0800

    Add SpringBoot Dubbo

---
+++ src/main/java/com/gufang/oa/bean/A2RInfo.java
@@ -0,0 +1,20 @@
+package com.gufang.oa.bean;
+
+import java.io.Serializable;
+
+public class A2RInfo implements Serializable{
+	private Integer aid = null;
+	private Integer rid = null;
+	public Integer getAid() {
+		return aid;
+	}
+	public void setAid(Integer aid) {
+		this.aid = aid;
+	}
+	public Integer getRid() {
+		return rid;
+	}
+	public void setRid(Integer rid) {
+		this.rid = rid;
+	}
+}
---
+++ src/main/java/com/test/dubbo/IUserMng.java
@@ -0,0 +1,5 @@
+package com.test.dubbo;
+
+public interface IUserMng {
+	public UserInfo getUserName(String id);
+}
